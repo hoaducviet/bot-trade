4645a40e539d0bba71e64b4fcfe2bc7f67f73014
hoaducviet <viethoaduc.21@gmail.com>
2025-03-20 21:30:08 +0700

    Add second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Hoa Duc Viet
           </li>
         </ol>
 
